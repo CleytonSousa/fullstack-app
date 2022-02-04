31ca8abc814df753f4524a93910a0c8b6df65742
Cleyton sousa <cleytonsouza808@gmail.com>
2022-02-04 16:13:31 -0300

    Product insert, update, delete

---
+++ Back/src/main/java/br/com/springboot/services/CategoryService.java
@@ -0,0 +1,19 @@
+package br.com.springboot.services;
+
+import br.com.springboot.entities.Category;
+import br.com.springboot.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll(){
+        return repository.findAll();
+    }
+}
